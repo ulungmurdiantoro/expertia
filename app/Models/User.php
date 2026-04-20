f88985c32c0ed1fddd62d585c714636c81ca9793
ulung murdiantoro <ulungmurdiantoro@gmail.com>
2026-04-20 09:25:18 +0700

    first comit

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Concerns\HasPublicId;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles, HasPublicId;
 
     /**
      * The attributes that are mass assignable.
@@ -19,9 +23,18 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'public_id',
         'name',
+        'username',
+        'profile_slug',
         'email',
         'password',
+        'avatar',
+        'bio',
+        'website',
+        'institution',
+        'social_links',
+        'status',
     ];
 
     /**
@@ -44,6 +57,47 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'social_links' => 'array',
         ];
+    }
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    public function follows(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            self::class,
+            'follows',
+            'follower_id',
+            'author_id'
+        )->withTimestamps();
+    }
+
+    public function followers(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            self::class,
+            'follows',
+            'author_id',
+            'follower_id'
+        )->withTimestamps();
+    }
+
+    public function bookmarks(): HasMany
+    {
+        return $this->hasMany(Bookmark::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function userNotifications(): HasMany
+    {
+        return $this->hasMany(UserNotification::class);
     }
 }
